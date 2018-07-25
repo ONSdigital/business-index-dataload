b28d0c007685919dd082226012ab5478ba259d55
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-07-25 13:44:34 +0100

    added trading style to test

--- src/main/scala/uk/gov/ons/bi/dataload/DataIngestionApps.scala
+++ src/main/scala/uk/gov/ons/bi/dataload/DataIngestionApps.scala
@@ -87,10 +87,9 @@ object LoadBiToEsApp extends DataloadApp {
 }
 
 object PreprocessLinksApp extends DataloadApp {
-    // Load Links JSON, preprocess data (apply UBRN etc), write to Parquet.
-
-    val sparkSess = SparkSession.builder.appName("ONS BI Dataload: Apply UBRN rules to Link data").enableHiveSupport.getOrCreate
-    val ctxMgr = new ContextMgr(sparkSess)
-    val lpp = new LinksPreprocessor(ctxMgr).loadAndPreprocessLinks(appConfig)
+  // Load Links JSON, preprocess data (apply UBRN etc), write to Parquet.
+  val sparkSess = SparkSession.builder.appName("ONS BI Dataload: Apply UBRN rules to Link data").enableHiveSupport.getOrCreate
+  val ctxMgr = new ContextMgr(sparkSess)
+  val lpp = new LinksPreprocessor(ctxMgr)
 }
 
--- src/test/scala/uk/gov/ons/bi/dataload/utils/RecordTransformersFlatSpec.scala
+++ src/test/scala/uk/gov/ons/bi/dataload/utils/RecordTransformersFlatSpec.scala
@@ -470,8 +470,8 @@ class RecordTransformersFlatSpec extends FlatSpec with ShouldMatchers {
     withCol.select("id", "BusinessName", "PostCode", "IndustryCode", "LegalStatus", "TradingStatus", "Turnover", "EmploymentBands", "CompanyNo", "VatRefs", "PayeRefs",
       "Address1", "Address2","Address3","Address4", "Address5", "TradingStyle").show
 
-    val results = withCol.select("Address1", "Address2","Address3","Address4", "Address5").collect().map(_.toString())
-    val expected =  Array("[VatAddress,add2,add3,null,null]")
+    val results = withCol.select("Address1", "Address2","Address3","Address4", "Address5", "TradingStyle").collect().map(_.toString())
+    val expected =  Array("[VatAddress,add2,add3,null,null,trading as Paye]")
 
     results shouldBe expected
   }
