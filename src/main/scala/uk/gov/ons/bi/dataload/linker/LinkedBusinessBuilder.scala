2c8baab04aaffebd1f2f78e3f8c805a8baf78ca4
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-07-25 09:51:41 +0100

    Updated tests and dataframe creation for address and trading style

--- src/main/scala/uk/gov/ons/bi/dataload/linker/LinkedBusinessBuilder.scala
+++ src/main/scala/uk/gov/ons/bi/dataload/linker/LinkedBusinessBuilder.scala
@@ -47,7 +47,8 @@ object LinkedBusinessBuilder {
       .withColumnRenamed("EmploymentBand","EmploymentBands")
 
     // Reorder the fields into the correct order
-    val biDf3: DataFrame = biDf2.select("id", "BusinessName", "UPRN", "PostCode", "IndustryCode", "LegalStatus", "TradingStatus", "Turnover", "EmploymentBands", "CompanyNo", "VatRefs", "PayeRefs" )
+    val biDf3: DataFrame = biDf2.select("id", "BusinessName", "UPRN", "PostCode", "IndustryCode", "LegalStatus", "TradingStatus", "Turnover", "EmploymentBands", "CompanyNo", "VatRefs", "PayeRefs",
+    "Address1", "Address2","Address3","Address4", "Address5", "TradingStyle")
 
     // Write BI DataFrame to Parquet file. We will load it into ElasticSearch separately.
 
