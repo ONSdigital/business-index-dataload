3a32766d2970ac17200f5f381fe89654a497fef6
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-08-13 11:45:16 +0100

    test run without previous links

--- src/main/scala/uk/gov/ons/bi/dataload/ubrn/LinksPreprocessor.scala
+++ src/main/scala/uk/gov/ons/bi/dataload/ubrn/LinksPreprocessor.scala
@@ -42,8 +42,8 @@ class LinksPreprocessor(ctxMgr: ContextMgr) {
     // WARNING:
     // UUID is generated when data is materialised e.g. in a SELECT statement,
     // so we need to PERSIST this data once we've added GID to fix it in place.
-    val newLinks = jsonLinks.withColumn("GID", generateUuid())
-    newLinks.persist(StorageLevel.MEMORY_AND_DISK)
+    //val newLinks = jsonLinks.withColumn("GID", generateUuid())
+    //newLinks.persist(StorageLevel.MEMORY_AND_DISK)
 
     // Parquet file locations from configuration (or runtime params)
     val appDataConfig = appConfig.AppDataConfig
@@ -56,46 +56,46 @@ class LinksPreprocessor(ctxMgr: ContextMgr) {
     val newLinksFileParquetPath = s"$workingDir/$linksFile"
 
     // Get previous links
-    val previousLinkStore = new PreviousLinkStore(ctxMgr)
-    val prevLinks = previousLinkStore.readFromSourceFile(prevLinksFileParquetPath)
+    //val previousLinkStore = new PreviousLinkStore(ctxMgr)
+    //val prevLinks = previousLinkStore.readFromSourceFile(prevLinksFileParquetPath)
 
     // Initialise LinkMatcher
-    val matcher = new LinkMatcher(ctxMgr)
+    //val matcher = new LinkMatcher(ctxMgr)
 
     // Apply all matching rules and get (matched, unmatched) records back
-    val (withOldUbrn, needUbrn) = matcher.applyAllMatchingRules(newLinks, prevLinks)
+    //val (withOldUbrn, needUbrn) = matcher.applyAllMatchingRules(newLinks, prevLinks)
 
     // ------------------------------
     // Now we can set new UBRNs for unmatched records
     // ------------------------------
     // Set new UBRN for these (start from max value from previous links)
 
-    val maxUrbn = UbrnManager.getMaxUbrn(prevLinks)
+    //val maxUrbn = UbrnManager.getMaxUbrn(prevLinks)
 
-    val withNewUbrn: DataFrame = UbrnManager.applyNewUbrn(needUbrn, maxUrbn)
+    val withNewUbrn: DataFrame = UbrnManager.applyNewUbrn(jsonLinks)
 
     // Finally, reconstruct full set of Links so we can save them all to Parquet
-    val linksToSave = matcher.combineLinksToSave(withOldUbrn, withNewUbrn)
+    //val linksToSave = matcher.combineLinksToSave(withOldUbrn, jsonLinks)
 
     // Cache the results because we want to write them to multiple files
     //linksToSave.persist(StorageLevel.MEMORY_AND_DISK)
 
     // Clear cached data we no longer need
-    prevLinks.unpersist()
-    newLinks.unpersist()
+    //prevLinks.unpersist()
+    //newLinks.unpersist()
 
     // Write preprocessed Links data to a Parquet output file ready for subsequent processing
-    jsonReader.writeParquet(linksToSave, newLinksFileParquetPath)
+    jsonReader.writeParquet(withNewUbrn, newLinksFileParquetPath)
 
     // We will also write a copy of the new preprocessed Links data to the "previous" dir:
     // 1. As e.g. LINKS_Output.parquet so we can easily pick it up next time
-    previousLinkStore.writeAsPrevLinks(appConfig, linksToSave)
+    //previousLinkStore.writeAsPrevLinks(appConfig, withNewUbrn)
 
     // 2. As e.g. 201703081145/LINKS_Output.parquet so it does not get over-written later
-    previousLinkStore.writeAsPrevLinks(appConfig, linksToSave, true)
+    //previousLinkStore.writeAsPrevLinks(appConfig, withNewUbrn, true)
 
     // Clear cached data we no longer need
-    linksToSave.unpersist()
+    withNewUbrn.unpersist()
   }
 
 }
