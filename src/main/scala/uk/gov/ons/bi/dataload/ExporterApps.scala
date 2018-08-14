271fa6d0a07328030887a29d333dc464ac5106a3
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-08-14 10:43:31 +0100

    test preprocess

--- src/main/scala/uk/gov/ons/bi/dataload/ExporterApps.scala
+++ src/main/scala/uk/gov/ons/bi/dataload/ExporterApps.scala
@@ -11,14 +11,14 @@ import uk.gov.ons.bi.dataload.utils.ContextMgr
 /**
   * Created by websc on 29/06/2017.
   */
-object HmrcBiExportApp extends Serializable with DataloadApp {
-
-  // val sparkConf = new SparkConf().setAppName("Export Business Index to CSV for HMRC")
-  val sparkSess = SparkSession.builder.appName("Export Business Index to CSV for HMRC").enableHiveSupport.getOrCreate
-  // ContextMgr provides our app-specific Spark context stuff
-  val ctxMgr = new ContextMgr(sparkSess)
-
-  // Run the HMRC extraction
-  HmrcBiCsvExtractor.extractBiToCsv(ctxMgr, appConfig)
-
-}
+//object HmrcBiExportApp extends Serializable with DataloadApp {
+//
+//  // val sparkConf = new SparkConf().setAppName("Export Business Index to CSV for HMRC")
+//  val sparkSess = SparkSession.builder.appName("Export Business Index to CSV for HMRC").enableHiveSupport.getOrCreate
+//  // ContextMgr provides our app-specific Spark context stuff
+//  val ctxMgr = new ContextMgr(sparkSess)
+//
+//  // Run the HMRC extraction
+//  HmrcBiCsvExtractor.extractBiToCsv(ctxMgr, appConfig)
+//
+//}
