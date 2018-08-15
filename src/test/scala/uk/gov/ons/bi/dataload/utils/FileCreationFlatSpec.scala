9421d792b2174f8d7495ea4991df69c1cb36f6ed
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-08-15 15:22:41 +0100

    added test for testing preprocess reads and generates a parquet file

---
+++ src/test/scala/uk/gov/ons/bi/dataload/utils/FileCreationFlatSpec.scala
@@ -0,0 +1,37 @@
+package uk.gov.ons.bi.dataload.utils
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.scalatest.{FlatSpec, Matchers}
+import java.io.File
+
+import uk.gov.ons.bi.dataload.ubrn.UbrnManager
+
+/**
+  * Created by ChiuA on 15/08/2018.
+  */
+class FileCreationFlatSpec extends FlatSpec with Matchers {
+
+  "A Links File " should "be read in from a parquet file and return a dataframe" in {
+
+    // setup config
+    val sparkSession: SparkSession = SparkSession.builder().master("local").getOrCreate()
+
+    val homeDir: String = "/Users/ChiuA/projects/dataload/business-index-dataload/src/main/resources"
+    val outputFilePath: String = s"$homeDir/output.parquet"
+    val inputFilePath: String  = s"$homeDir/input.parquet"
+
+    // Used to create initial input parquet file
+    //val jsonPath = s"$homeDir/links.json"
+
+    new File(outputFilePath).delete()
+
+    val df: DataFrame = sparkSession.read.parquet(inputFilePath)
+
+    val withNewUbrn = UbrnManager.applyNewUbrn(df)
+
+    withNewUbrn.write.mode("overwrite").parquet(outputFilePath)
+
+    val result = new File(outputFilePath).exists
+    result shouldBe true
+  }
+}
