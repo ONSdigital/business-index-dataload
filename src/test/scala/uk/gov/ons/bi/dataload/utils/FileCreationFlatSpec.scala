f1cabe7e950d294c596c850062fd8a9fe3a80ac5
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-08-17 13:02:37 +0100

    link data app file creation unit test added

--- src/test/scala/uk/gov/ons/bi/dataload/utils/FileCreationFlatSpec.scala
+++ src/test/scala/uk/gov/ons/bi/dataload/utils/FileCreationFlatSpec.scala
@@ -2,20 +2,19 @@ package uk.gov.ons.bi.dataload.utils
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.{FlatSpec, Matchers}
-
-import uk.gov.ons.bi.dataload.model._
 import java.io.File
 
-import uk.gov.ons.bi.dataload.PreprocessLinksApp.appConfig
+import uk.gov.ons.bi.dataload.linker.LinkedBusinessBuilder
 import uk.gov.ons.bi.dataload.loader.SourceDataToParquetLoader
 import uk.gov.ons.bi.dataload.ubrn.UbrnManager
+import uk.gov.ons.bi.dataload.model._
 
 /**
   * Created by ChiuA on 15/08/2018.
   */
 class FileCreationFlatSpec extends FlatSpec with Matchers {
 
-  "A Links File " should "be read in from a parquet file and return a dataframe" in {
+  "A Links File " should "be read in from a parquet file and return a dataframe containing links with UBRNS" in {
 
     // setup config
     val sparkSession: SparkSession = SparkSession.builder().master("local").getOrCreate()
@@ -47,7 +46,7 @@ class FileCreationFlatSpec extends FlatSpec with Matchers {
     result shouldBe true
   }
 
-  "Admin source files " should "read in and wrote out as a parquet file for the admin source CH" in {
+  "Admin source files " should "read in and write out as a parquet file for the admin source CH" in {
 
     val sparkSession: SparkSession = SparkSession.builder().master("local").getOrCreate()
     val appConfig: AppConfig = new AppConfig
@@ -67,7 +66,7 @@ class FileCreationFlatSpec extends FlatSpec with Matchers {
     result shouldBe true
   }
 
-  "Admin source files " should "read in and wrote out as a parquet file for the admin source PAYE" in {
+  "Admin source files " should "read in and write out as a parquet file for the admin source PAYE" in {
     val sparkSession: SparkSession = SparkSession.builder().master("local").getOrCreate()
     val appConfig: AppConfig = new AppConfig
     val ctxMgr = new ContextMgr(sparkSession)
@@ -86,7 +85,7 @@ class FileCreationFlatSpec extends FlatSpec with Matchers {
     result shouldBe true
   }
 
-  "Admin source files " should "read in and wrote out as a parquet file for the admin source VAT" in {
+  "Admin source files " should "read in and write out as a parquet file for the admin source VAT" in {
     val sparkSession: SparkSession = SparkSession.builder().master("local").getOrCreate()
     val appConfig: AppConfig = new AppConfig
     val ctxMgr = new ContextMgr(sparkSession)
@@ -105,7 +104,7 @@ class FileCreationFlatSpec extends FlatSpec with Matchers {
     result shouldBe true
   }
 
-  "Admin source files " should "read in and wrote out as a parquet file for the admin source TCN-lookup" in {
+  "Admin source files " should "read in and write out as a parquet file for the admin source TCN-lookup" in {
     val sparkSession: SparkSession = SparkSession.builder().master("local").getOrCreate()
     val appConfig: AppConfig = new AppConfig
     val ctxMgr = new ContextMgr(sparkSession)
@@ -116,14 +115,33 @@ class FileCreationFlatSpec extends FlatSpec with Matchers {
     val parquetFile = appConfig.AppDataConfig.tcn
     val targetFilePath = s"$workingDir/$parquetFile"
 
-    new File(targetFilePath).delete()
+    new File(targetFilePath).delete
 
     sourceDataLoader.loadTcnToSicCsvLookupToParquet(appConfig)
 
     val result = new File(targetFilePath).exists
-    result shouldBe true  }
+    result shouldBe true
+  }
 
-  "LinkDataApp"
+  "LinkDataApp " should "read in ubrn links and admin data and outputs parquet file containing fully populated Legal Units" in {
+    val sparkSession: SparkSession = SparkSession.builder().master("local").getOrCreate()
+    val appConfig: AppConfig = new AppConfig
+    val ctxMgr = new ContextMgr(sparkSession)
 
-  "LoadBiToEsApp"
+    val appDataConfig = appConfig.AppDataConfig
+    val workDir = appDataConfig.workingDir
+    val parquetBiFile = appDataConfig.bi
+    val biFile = s"$workDir/$parquetBiFile"
+
+    new File(biFile).delete
+
+    LinkedBusinessBuilder.buildLinkedBusinessIndexRecords(ctxMgr, appConfig)
+
+    val result = new File(biFile).exists
+    result shouldBe true
+  }
+
+  "LoadBiToEsApp " should "populate elasticsearch with the populated legal units "in {
+
+  }
 }
