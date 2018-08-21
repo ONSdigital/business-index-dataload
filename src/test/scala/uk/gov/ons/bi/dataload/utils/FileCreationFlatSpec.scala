074530627e10190f5e2f4ea67b038332bf01559c
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-08-21 09:06:17 +0100

    dataframe test for pre process

--- src/test/scala/uk/gov/ons/bi/dataload/utils/FileCreationFlatSpec.scala
+++ src/test/scala/uk/gov/ons/bi/dataload/utils/FileCreationFlatSpec.scala
@@ -1,6 +1,6 @@
 package uk.gov.ons.bi.dataload.utils
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.scalatest.{FlatSpec, Matchers}
 import java.io.File
 
@@ -12,40 +12,55 @@ import uk.gov.ons.bi.dataload.model._
 /**
   * Created by ChiuA on 15/08/2018.
   */
+
 class FileCreationFlatSpec extends FlatSpec with Matchers {
 
   "A Links File " should "be read in from a parquet file and return a dataframe containing links with UBRNS" in {
 
     // setup config
     val sparkSession: SparkSession = SparkSession.builder().master("local").getOrCreate()
+    import sparkSession.implicits._
     val appConfig: AppConfig = new AppConfig
     val ctxMgr = new ContextMgr(sparkSession)
 
-    new LinksPreprocessor(ctxMgr).loadAndPreprocessLinks(appConfig)
-
+    // get input path
     val linksDir = appConfig.OnsDataConfig.linksDataConfig.dir
     val linksFile = appConfig.OnsDataConfig.linksDataConfig.parquet
+    val inputFilePath: String  = s"$linksDir/$linksFile"
 
+    // get output path
     val outputDir = appConfig.AppDataConfig.workingDir
     val outputFile = appConfig.AppDataConfig.links
-
     val outputFilePath: String = s"$outputDir/$outputFile"
-    val inputFilePath: String  = s"$linksDir/$linksFile"
+
+    // delete and create output parquet
+    new File(outputFilePath).delete()
+    new LinksPreprocessor(ctxMgr).loadAndPreprocessLinks(appConfig)
 
     // Used to create initial input parquet file
     val jsonPath = s"$linksDir/links.json"
-
-    new File(outputFilePath).delete()
-
     sparkSession.read.json(jsonPath).write.mode("overwrite").parquet(inputFilePath)
 
-    val result = new File(outputFilePath).exists
-    result shouldBe true
+    //val result = new File(outputFilePath).exists
+
+    // Read in created parquet
+    val results = sparkSession.read.parquet(outputFilePath).collect()
+
+    val expected = Seq(
+      (Array("ch1"),Array("065H7Z31732"),Array("868500288000"),1000000000000001L),
+      (Array("ch2"),Array(""),Array(""),1000000000000002L),
+      (Array(""),Array("035H7A22627"),Array("868504062000"),1000000000000003L),
+      (Array("ch3"),Array(""),Array("862764963000"),1000000000000004L),
+      (Array("ch4"),Array("125H7A71620"),Array(""),1000000000000005L)
+    ).toDF("CH","PAYE","VAT","UBRN").collect()
+
+    results shouldBe expected
   }
 
   "Admin source files " should "read in and write out as a parquet file for the admin source CH" in {
 
     val sparkSession: SparkSession = SparkSession.builder().master("local").getOrCreate()
+    import sparkSession.implicits._
     val appConfig: AppConfig = new AppConfig
     val ctxMgr = new ContextMgr(sparkSession)
     val sourceDataLoader = new SourceDataToParquetLoader(ctxMgr)
