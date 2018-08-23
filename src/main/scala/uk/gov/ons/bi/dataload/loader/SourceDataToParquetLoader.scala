249e6d9a9472c85b15b07af1955a229769e80bd6
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-08-23 14:58:32 +0100

    testing relative pathing

--- src/main/scala/uk/gov/ons/bi/dataload/loader/SourceDataToParquetLoader.scala
+++ src/main/scala/uk/gov/ons/bi/dataload/loader/SourceDataToParquetLoader.scala
@@ -27,25 +27,42 @@ class SourceDataToParquetLoader(ctxMgr: ContextMgr) {
     val appDataConfig = appConfig.AppDataConfig
     val workingDir = appDataConfig.workingDir
 
-    // Get directories and file names etc for specified data source
-    val (extSrcFile, extDataDir, parquetFile, tempTable) = biSource match {
-      case VAT => (extDataConfig.vat, extDataConfig.vatDir, appDataConfig.vat, "temp_vat")
-      case CH => (extDataConfig.ch, extDataConfig.chDir, appDataConfig.ch, "temp_ch")
-      case PAYE => (extDataConfig.paye, extDataConfig.payeDir, appDataConfig.paye, "temp_paye")
-    }
+     //Get directories and file names etc for specified data source
+        val (extSrcFile, extDataDir, parquetFile, tempTable) = biSource match {
+          case VAT => (extDataConfig.vat, extDataConfig.vatDir, appDataConfig.vat, "temp_vat")
+          case CH => (extDataConfig.ch, extDataConfig.chDir, appDataConfig.ch, "temp_ch")
+          case PAYE => (extDataConfig.paye, extDataConfig.payeDir, appDataConfig.paye, "temp_paye")
+        }
 
-    val extSrcFilePath = s"/$extEnv/$extBaseDir/$extDataDir/$extSrcFile"
-    log.info(s"Reading $biSource data from: $extSrcFilePath")
+    val inputPath = s"/$extEnv/$extBaseDir/$extDataDir/$extSrcFile"
+    val outputPath = s"$workingDir/$parquetFile"
+    writeAdminParquet(inputPath, outputPath, tempTable, biSource)
+  }
+
+  def writeAdminParquet(inputPath: String, outputPath: String, tempTable: String, biSource: BusinessDataSource) = {
+    log.info(s"Reading $biSource data from: $inputPath")
 
     // Get corresponding reader based on BIDataSource
     val reader = new CsvReader(ctxMgr, tempTable)
 
     // Process the data
-    val data = reader.readFromAdminSourceFile(extSrcFilePath, biSource)
-    val targetFilePath = s"$workingDir/$parquetFile"
-    log.info(s"Writing $biSource data to: $targetFilePath")
+    val data = reader.readFromAdminSourceFile(inputPath, biSource)
+    log.info(s"Writing $biSource data to: $outputPath")
 
-    reader.writeParquet(data, targetFilePath)
+    reader.writeParquet(data, outputPath)
+  }
+
+  def writeTCN(inputPath: String, outputPath: String) = {
+    log.info(s"Reading TCN-SIC lookup from: $inputPath")
+    // Get CSV reader for this data source
+    val reader: BIDataReader = new CsvReader(ctxMgr, "temp_tcn")
+
+    // Process the data
+    val data = reader.readFromSourceFile(inputPath)
+
+    log.info(s"Writing TCN-SIC lookup to: $outputPath")
+
+    reader.writeParquet(data, outputPath)
   }
 
 
@@ -66,20 +83,9 @@ class SourceDataToParquetLoader(ctxMgr: ContextMgr) {
     val parquetFile = appDataConfig.tcn
 
     val extSrcFilePath = s"/$lookupsEnv/$lookupsDir/$tcnToSicFile"
-
-    log.info(s"Reading TCN-SIC lookup from: $extSrcFilePath")
-
-    // Get CSV reader for this data source
-    val reader: BIDataReader = new CsvReader(ctxMgr, "temp_tcn")
-
-    // Process the data
-    val data = reader.readFromSourceFile(extSrcFilePath)
-
     val targetFilePath = s"$workingDir/$parquetFile"
 
-    log.info(s"Writing TCN-SIC lookup to: $targetFilePath")
-
-    reader.writeParquet(data, targetFilePath)
+    writeTCN(extSrcFilePath, targetFilePath)
   }
 
   def loadSourceBusinessDataToParquet(appConfig: AppConfig) = {
