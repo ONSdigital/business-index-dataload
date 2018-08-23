17c662835af8ea9a5d6a697210a3c2d0d9e973b4
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-08-23 11:15:46 +0100

    refactor quoteall method

--- src/main/scala/uk/gov/ons/bi/dataload/writer/BiCsvWriter.scala
+++ src/main/scala/uk/gov/ons/bi/dataload/writer/BiCsvWriter.scala
@@ -19,6 +19,8 @@ object BiCsvWriter {
 
     outputDf.write.mode("overwrite")
         .option("header", "true")
+        .option("quote","\"")
+        //.option("quoteMode","ALL")
         .option("quoteAll", "true")
         .option("nullValue","")
         .csv(outputFile)
