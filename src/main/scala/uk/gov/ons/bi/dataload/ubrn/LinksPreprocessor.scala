9e472ea5b144683530bc4931315e3939fc875cfb
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-08-31 09:19:13 +0100

    split up preprocess method

--- src/main/scala/uk/gov/ons/bi/dataload/ubrn/LinksPreprocessor.scala
+++ src/main/scala/uk/gov/ons/bi/dataload/ubrn/LinksPreprocessor.scala
@@ -4,7 +4,7 @@ import java.util.UUID
 
 import com.google.inject.Singleton
 
-import uk.gov.ons.bi.dataload.reader.{LinksFileReader, PreviousLinkStore}
+import uk.gov.ons.bi.dataload.reader.{BIDataReader, PreviousLinkStore}
 import uk.gov.ons.bi.dataload.utils.{AppConfig, ContextMgr}
 import org.apache.spark.sql.functions.udf
 import org.apache.spark.sql.expressions.UserDefinedFunction
@@ -14,12 +14,12 @@ import org.apache.spark.sql.expressions.UserDefinedFunction
   */
 
 @Singleton
-class LinksPreprocessor(ctxMgr: ContextMgr) {
+class LinksPreprocessor(ctxMgr: ContextMgr) extends PreviousLinkStore(ctxMgr) with BIDataReader{
 
   // Create UDF to generate a UUID
   val generateUuid: UserDefinedFunction = udf(() => UUID.randomUUID().toString)
 
-  def getNewLinksDataFromParquet(reader: LinksFileReader , appConfig: AppConfig): DataFrame = {
+  def getNewLinksDataFromParquet(appConfig: AppConfig): DataFrame = {
 
     // get source/target directories
     val linksDataConfig = appConfig.OnsDataConfig.linksDataConfig
@@ -28,17 +28,40 @@ class LinksPreprocessor(ctxMgr: ContextMgr) {
     val parquetFilePath = s"$dataDir/$parquetFile"
 
     // Load the JSON links data
-    reader.readFromSourceFile(parquetFilePath)
+    readFromSourceFile(parquetFilePath)
   }
 
-  def loadAndPreprocessLinks(appConfig: AppConfig) = {
+  // apply ubrn and write to parquet
+  def applyUBRN(prevLinks: DataFrame, parquetLinks: DataFrame) = {
 
+    val maxUrbn = UbrnManager.getMaxUbrn(prevLinks)
+
+    //Set new UBRN for these (start from max value from previous links)
+    val withNewUbrn: DataFrame = UbrnManager.applyNewUbrn(parquetLinks, maxUrbn)
+    withNewUbrn
+  }
+
+  def writeLinksToParquet(withNewUbrn: DataFrame, outputPath: String, appConfig: AppConfig) = {
+    //write output to hdfs
+    writeParquet(withNewUbrn, outputPath)
+
+    // We will also write a copy of the new preprocessed Links data to the "previous" dir:
+    // 1. As e.g. LINKS_Output.parquet so we can easily pick it up next time
+    writeAsPrevLinks(appConfig, withNewUbrn)
+
+    // 2. As e.g. 201703081145/LINKS_Output.parquet so it does not get over-written later
+    writeAsPrevLinks(appConfig, withNewUbrn, true)
+  }
+
+  def loadLinksToDF(appConfig: AppConfig): (DataFrame, DataFrame, String) = {
+    // Lot of caching needed here, so we cache to disk and memory
+    // Load the new Links from JSON
     val appDataConfig = appConfig.AppDataConfig
 
     // get directory
     val workingDir = appDataConfig.workingDir
-    val linksFile = appDataConfig.links
     val prevDir = appDataConfig.prevDir
+    val linksFile = appDataConfig.links
 
     val linksDataConfig = appConfig.OnsDataConfig.linksDataConfig
     val dataDir = linksDataConfig.dir
@@ -50,12 +73,16 @@ class LinksPreprocessor(ctxMgr: ContextMgr) {
     val prevLinksFileParquetPath = s"$prevDir/$linksFile"
 
     // read links from parquet
-    val LinksReader = new LinksFileReader(ctxMgr)
-    val parquetLinks  =  getNewLinksDataFromParquet(LinksReader, appConfig)
+    val parquetLinks = getNewLinksDataFromParquet(appConfig)
 
     // Get previous links
-    val previousLinkStore = new PreviousLinkStore(ctxMgr)
-    val prevLinks = previousLinkStore.readFromSourceFile(prevLinksFileParquetPath)
+    val prevLinks = readFromSourceFile(prevLinksFileParquetPath)
+    (parquetLinks, prevLinks, outputPath)
+  }
+
+  def loadAndPreprocessLinks(appConfig: AppConfig) = {
+
+    val (parquetLinks, prevLinks, outputPath) = loadLinksToDF(appConfig)
 
     // WARNING:
     // UUID is generated when data is materialised e.g. in a SELECT statement,
@@ -68,26 +95,14 @@ class LinksPreprocessor(ctxMgr: ContextMgr) {
 
     // Apply all matching rules and get (matched, unmatched) records back
     val (withOldUbrn, needUbrn) = matcher.applyAllMatchingRules(newLinks, prevLinks)
-
-    val maxUrbn = UbrnManager.getMaxUbrn(prevLinks)
-
-    //Set new UBRN for these (start from max value from previous links)
-    val withNewUbrn: DataFrame = UbrnManager.applyNewUbrn(parquetLinks, maxUrbn)
+    val withNewUbrn = applyUBRN(prevLinks, parquetLinks)
 
     // Finally, reconstruct full set of Links so we can save them all to Parquet
     val linksToSave = matcher.combineLinksToSave(withOldUbrn, withNewUbrn)
     // Cache the results because we want to write them to multiple files
     linksToSave.persist(StorageLevel.MEMORY_AND_DISK)
 
-    //write output to hdfs
-    LinksReader.writeParquet(withNewUbrn, outputPath)
-
-    // We will also write a copy of the new preprocessed Links data to the "previous" dir:
-    // 1. As e.g. LINKS_Output.parquet so we can easily pick it up next time
-    previousLinkStore.writeAsPrevLinks(appConfig, withNewUbrn)
-
-    // 2. As e.g. 201703081145/LINKS_Output.parquet so it does not get over-written later
-    previousLinkStore.writeAsPrevLinks(appConfig, withNewUbrn, true)
+    writeLinksToParquet(withNewUbrn, outputPath, appConfig)
 
     linksToSave.unpersist()
     newLinks.unpersist()
