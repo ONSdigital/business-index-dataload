80be1a949c128b60a1012057b448988a563d62db
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-09-11 12:27:11 +0100

    added methods for fixing sic codes for ch

--- src/main/scala/uk/gov/ons/bi/dataload/utils/FixSic.scala
+++ src/main/scala/uk/gov/ons/bi/dataload/utils/FixSic.scala
@@ -1,31 +1,81 @@
 package uk.gov.ons.bi.dataload.utils
 
-/**
-  * Created by chohab on 10/09/2018.
-  */
+import org.apache.spark.sql.SparkSession
+
 object FixSic {
 
-  val subForSic07 = Map {
-    "33120" -> "28302"
-    "28960" -> "28990"
-    "26301" -> "27900"
-    "33200" -> "26309"
-    "32500" -> "26701"
-    "96090" -> "33190"
-    "77210" -> "77299"
-    "81210" -> "81299s"
+  lazy val spark: SparkSession = {
+    SparkSession
+      .builder()
+      .master("local")
+      .appName("spark tests")
+      .getOrCreate()
   }
+
+  val colNames = Seq(
+    "SICCode.SicText_1",
+    "SICCode.SicText_2",
+    "SICCode.SicText_3",
+    "SICCode.SicText_4"
+  )
+
+  val validNonTradingSic = Seq(
+    "74990",
+    "98000",
+    "99000",
+    "99999"
+  )
 
   def createValidSicList(filepath: String): Seq[String] = {
-    Seq(filepath)
+
+    val sicCodeIndex = spark.read.option("header", "true").csv(filepath)
+    val sic07 = sicCodeIndex.select("SIC 2007").collect().map(x => x.toString()).toSeq.map(x => x.replaceAll("[\\[\\]]",""))
+    sic07
   }
 
-  def replaceIncorrectSic(x: String, sicList: List[String]): String = {
-    ""
+  def addValidNonTradingSic(sicList: Seq[String]): Seq[String] = {
+
+    val newList = sicList ++ validNonTradingSic
+    newList
   }
 
-  def fixFourDigitSic(x: String, sicList: List[String]): String = {
-    ""
+  def replaceIncorrectSic(sicField: String, sicList: Seq[String]): String = {
+
+    val space = sicField.split(" ")
+    val sicDigits = space(0)
+    val fiveDigitPattern = "(?<!\\d)\\d{5}(?!\\d)".r.pattern
+
+      if (sicDigits.length == 0) space(0)
+      else if (!fiveDigitPattern.matcher(sicDigits).matches) space(0) = "99999 - " + sicDigits
+      else if (!(sicList contains sicDigits)) space(0) = "99999"
+      else space(0) = sicDigits match {
+        case "33120" => "28302"
+        case "28960" => "28990"
+        case "26301" => "27900"
+        case "33200" => "26309"
+        case "32500" => "26701"
+        case "96090" => "33190"
+        case "77210" => "77299"
+        case "81210" => "81299"
+        case _ => sicDigits
+      }
+    space.mkString(" ")
+  }
+
+  def fixFourDigitSic(sicField: String): String = {
+
+    val space = sicField.split(" ")
+    val sicDigits = space(0)
+    val fourDigitPattern = "(?<!\\d)\\d{4}(?!\\d)".r.pattern
+
+    val test = if (fourDigitPattern.matcher(sicDigits).matches) {
+      space(0) = "0" + space(0)
+      space.mkString(" ")
+    }
+    else {
+      sicField
+    }
+    test
   }
 
 }
--- src/test/scala/uk/gov/ons/bi/dataload/utils/FixSicFlatSpec.scala
+++ src/test/scala/uk/gov/ons/bi/dataload/utils/FixSicFlatSpec.scala
@@ -3,29 +3,66 @@ package uk.gov.ons.bi.dataload.utils
 import org.apache.spark.sql.SparkSession
 import org.scalatest.{FlatSpec, Matchers}
 
+import uk.gov.ons.bi.dataload.reader.LinksFileReader
+
 
 class FixSicFlatSpec extends FlatSpec with Matchers {
 
-  "A SIC list" should "contain all valid SICs that are possible in the IDBR" in {
+  val sparkSession: SparkSession = SparkSession.builder().master("local").getOrCreate()
+  val ctxMgr = new ContextMgr(sparkSession)
+  val parquetReader = new LinksFileReader(ctxMgr)
+  val homeDir = parquetReader.readFromLocal("/")
 
-    val sicList = FixSic.createValidSicList("/")
+  "createValidSicList" should "contain all valid SICs that are present in the sicCodeIndex" in {
+
+    val sicList = FixSic.createValidSicList(s"$homeDir/sicCodeIndex2017.csv")
     val result = sicList.length
     // Expected is taken from the 15,592 valid SICs published by ONS at
-    // plus the 4 valid SICs for non-trading companies (74990, 98000, 99000, 99999)
-    val expected = 15596
+    val expected = 15592
 
     result shouldBe expected
 
   }
 
-  "An incorrect SIC code" should "be replaced by a valid SIC code" in {
+  "addValidNonTradingSic" should "add valid Non Trading Sics to the existing sic list" in {
+    val sicList = FixSic.createValidSicList(s"$homeDir/sicCodeIndex2017.csv")
+    val result = FixSic.addValidNonTradingSic(sicList).length
+    // plus the 4 valid SICs for non-trading companies (74990, 98000, 99000, 99999)
+    val expected = 15596
 
-
+    result shouldBe expected
   }
 
+  "replaceIncorrectSic" should "be replaced by a valid SIC code" in {
+    val sicList = FixSic.createValidSicList(s"$homeDir/sicCodeIndex2017.csv")
+    val testString = "1234a"
+    val results = FixSic.replaceIncorrectSic(testString, sicList)
 
-  "A 4 digit SIC code" should "have a leading zero appended to it" in {
+    results shouldBe "99999 - 1234a"
+  }
 
+  "replaceIncorrectSic" should "when given a valid sic be replaced based on IDBR methodology" in {
+    val sicList = FixSic.createValidSicList(s"$homeDir/sicCodeIndex2017.csv")
+    val testString = "33120 - tester"
+    val results = FixSic.replaceIncorrectSic(testString, sicList)
+
+    results shouldBe "28302 - tester"
+  }
+
+  "fixFourDigitSic" should "have a leading zero appended to it" in {
+
+    val testString = "1234"
+    val results = FixSic.fixFourDigitSic(testString)
+
+    results shouldBe "01234"
+  }
+
+  "fixFourDigitSic" should "do nothing" in {
+
+    val testString = "12345"
+    val results = FixSic.fixFourDigitSic(testString)
+
+    results shouldBe "12345"
   }
 
 }
