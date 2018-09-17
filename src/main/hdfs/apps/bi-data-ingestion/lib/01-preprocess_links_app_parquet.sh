2f11215c4c40cde351e2f1350a2b4e8cf270ca8f
Sion Williams <sion5@hotmail.co.uk>
2018-09-17 10:55:55 +0100

    fixup! Jenkinsfile continuous delivery

--- src/main/hdfs/apps/bi-data-ingestion/lib/01-preprocess_links_app_parquet.sh
+++ src/main/hdfs/apps/bi-data-ingestion/lib/01-preprocess_links_app_parquet.sh
@@ -1,2 +1,18 @@
 #!/bin/bash​
-spark2-submit --class uk.gov.ons.bi.dataload.PreprocessLinksApp --master='yarn' --deploy-mode='cluster' --num-executors 6 --driver-memory 20G --executor-memory 40G --jars hdfs://prod1/${HOME}/businessIndex/lib/config-1.3.2.jar --driver-java-options "-DBI_DATALOAD_LINKS_DATA_DIR=${HOME}/bi-parquet -DBI_DATALOAD_APP_DATA_WORK=${HOME}/businessIndex/WORKINGDATA -Dbi-dataload.app-data.cluster=cluster" hdfs://prod1/${HOME}/businessIndex/lib/business-index-dataload_2.11-1.6.jar
+
+__workflow_name="bi-data-ingestion"
+__module_name="business-index-dataload"
+__hdfs_install_base="${OOZIE_HOME}/applications/oozie"
+__workflow_basedir="${__hdfs_install_base}/${__module_name}-latest/apps/${__workflow_name}"
+__workflow_libs="${__workflow_basedir}/lib"
+
+spark2-submit --class uk.gov.ons.bi.dataload.PreprocessLinksApp \
+    --master='yarn' \
+    --deploy-mode='cluster' \
+    --num-executors 6 \
+    --driver-memory 20G \
+    --executor-memory 40G \
+    --jars "hdfs://prod1/${__workflow_libs}/config-1.3.2.jar" \
+    --driver-java-options \
+    "-DBI_DATALOAD_LINKS_DATA_DIR=${OOZIE_HOME}/bi-parquet -DBI_DATALOAD_APP_DATA_WORK=${OOZIE_HOME}/businessIndex/WORKINGDATA -Dbi-dataload.app-data.cluster=cluster" \
+    hdfs://prod1/${__workflow_libs}/business-index-dataload-*.jar
