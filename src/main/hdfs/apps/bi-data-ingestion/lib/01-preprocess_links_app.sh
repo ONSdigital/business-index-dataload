7e3a338165c1aced02484a6099c49f757ccbf1cc
Ashutosh Jindal <Ash.Jindal@ext.ons.gov.uk>
2018-11-01 12:08:12 +0000

    Set OOZIE_HOME variable in scripts
    Currently, these scripts are failing since the OOZIE_HOME variable is
    not being resolved and consecutively the business-index-dataload*.jar is
    not being found by the spark-submit job.

--- src/main/hdfs/apps/bi-data-ingestion/lib/01-preprocess_links_app.sh
+++ src/main/hdfs/apps/bi-data-ingestion/lib/01-preprocess_links_app.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+OOZIE_HOME="/user/$USER"
+
 __workflow_name="bi-data-ingestion"
 __module_name="business-index-dataload"
 __hdfs_install_base="${OOZIE_HOME}/applications/oozie"
 __workflow_basedir="${__hdfs_install_base}/${__module_name}-latest/apps/${__workflow_name}"
 __workflow_libs="${__workflow_basedir}/lib"
+
+
+echo "__workflow_libs: ${__workflow_libs}"
 
 spark2-submit --class uk.gov.ons.bi.dataload.PreprocessLinksApp \
     --master='yarn' \
