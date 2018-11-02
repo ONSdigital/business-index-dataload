e3bc4cbac256931ecb1f276c87ef65c94130e76e
Ashutosh Jindal <Ash.Jindal@ext.ons.gov.uk>
2018-11-02 14:00:17 +0000

    Fix the jar being used in the spark2-submit command
    spark2-submit does not accept wildcards in the path of the jar which
    contains the class being executed. This change queries the workflow_lib
    folder to find the jar using an 'hdfs dfs -ls' command and then parses
    it to extract the complete path of the jar which is then used in the
    spark2-submit job

--- src/main/hdfs/apps/bi-data-ingestion/lib/04-load_bi_to_es_app.sh
+++ src/main/hdfs/apps/bi-data-ingestion/lib/04-load_bi_to_es_app.sh
@@ -9,6 +9,9 @@ __workflow_basedir="${__hdfs_install_base}/${__module_name}-latest/apps/${__work
 __workflow_libs="${__workflow_basedir}/lib"
 
 echo "__workflow_libs: ${__workflow_libs}"
+
+BI_DATALOAD_JAR=$( hdfs dfs -ls "hdfs://prod1/${__workflow_libs}/business-index-dataload-*.jar" | awk '{print $NF}' )
+
 spark2-submit --class uk.gov.ons.bi.dataload.LoadBiToEsApp \
     --master='yarn' \
     --deploy-mode='cluster' \
@@ -18,4 +21,4 @@ spark2-submit --class uk.gov.ons.bi.dataload.LoadBiToEsApp \
     --jars "hdfs://prod1/${__workflow_libs}/config-1.3.2.jar,hdfs://prod1/${__workflow_libs}/elasticsearch-spark-20_2.11-6.0.0.jar" \
     --driver-java-options \
     "-Dbi-dataload.es.nodes=$1 -Dbi-dataload.es.index=$2 -DBI_DATALOAD_ENV=${OOZIE_HOME} -DBI_DATALOAD_CLUSTER=cluster -DBI_DATALOAD_YEAR=$3 -DBI_DATALOAD_MONTH=$4" \
-    hdfs://prod1/${__workflow_libs}/business-index-dataload-*.jar
+    $BI_DATALOAD_JAR
